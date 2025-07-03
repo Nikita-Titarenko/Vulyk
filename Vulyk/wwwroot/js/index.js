41e99632bee516391c2f221ed5c73c13f82f555b
Nikita Titarenko <nikitatitarenko81@gmail.com>
2025-07-03 15:18:18 +0300

    comments page, start footer

---
+++ Vulyk/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿const commentsWrapper = document.getElementById('comments-wrapper');
+
+const previousComment = document.getElementById('previous-comment').addEventListener('click', () => {
+    const style = getComputedStyle(document.querySelector('.comment'));
+    scroll(-(parseFloat(style.width) + parseFloat(style.marginLeft) + parseFloat(style.marginRight)));
+});
+
+const nextComment = document.getElementById('next-comment').addEventListener('click', () => {
+    const style = getComputedStyle(document.querySelector('.comment'));
+    scroll(parseFloat(style.width) + parseFloat(style.marginLeft) + parseFloat(style.marginRight));
+});
+
+function scroll(scroll) {
+    commentsWrapper.scrollBy({ left: scroll, behavior: 'smooth' });
+}
